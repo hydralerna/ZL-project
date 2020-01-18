d6294bfcce9116ddd10d1f680e9d56c8c397255d
hydralerna <loic.guyader.froggy@gmail.com>
2020-01-18 23:21:21 +0100

    Adding a metatable to set the camera size in order to avoid problems with the hud

---
+++ skylands-of-solarus/data/scripts/meta/map.lua
@@ -0,0 +1,15 @@
+-- Initialize Map behavior specific to this quest.
+
+-- Variables
+local map_meta = sol.main.get_metatable("map")
+
+-- Include scripts
+require ("scripts/multi_events")
+
+-- Set the camera size to avoid problems with the hud
+map_meta:register_event("on_started", function(map)
+
+  local camera = map:get_camera()
+  camera:set_position_on_screen(0, 16)
+  camera:set_size(208, 160)
+end)
