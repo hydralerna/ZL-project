0e17d46940068e390e13f2bb1b7f9c50ef2956e4
hydralerna <loic.guyader.froggy@gmail.com>
2020-01-18 23:23:00 +0100

    Add a metatable to get more directions for the sprite

---
+++ skylands-of-solarus/data/scripts/meta/explosion.lua
@@ -0,0 +1,15 @@
+-- Initialize Explosion behavior specific to this quest.
+
+-- Variables
+local explosion_meta = sol.main.get_metatable("explosion")
+
+-- Include scripts
+require ("scripts/multi_events")
+
+-- Allows you to have more "directions" for the sprite, here chosen randomly.
+function explosion_meta:on_created()
+  
+  local explosion = self
+  local sprite = explosion:get_sprite()
+  sprite:set_direction(math.random(0, 1))
+end
