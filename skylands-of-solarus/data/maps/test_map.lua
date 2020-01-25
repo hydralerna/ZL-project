8f3106ec76ae34498a81faf0bcb79a8f8e9d6955
hydralerna <loic.guyader.froggy@gmail.com>
2020-01-25 23:56:20 +0100

    Update test_map.lua

--- skylands-of-solarus/data/maps/test_map.lua
+++ skylands-of-solarus/data/maps/test_map.lua
@@ -7,15 +7,18 @@
 -- See the Solarus Lua API documentation:
 -- http://www.solarus-games.org/doc/latest
 
+require("scripts/multi_events")
+
 local map = ...
 local game = map:get_game()
 
 -- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
+map:register_event("on_started", function()
 
   -- You can initialize the movement and sprites of various
   -- map entities here.
-end
+
+end)
 
 -- Event called after the opening transition effect of the map,
 -- that is, when the player takes control of the hero.
