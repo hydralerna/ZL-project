fb272af0f713d31da992db055438929f0cfb90ce
hydralerna <loic.guyader.froggy@gmail.com>
2021-08-13 22:42:39 +0200

    Update language_manager.lua

--- skylands-of-solarus/data/scripts/language_manager.lua
+++ skylands-of-solarus/data/scripts/language_manager.lua
@@ -5,7 +5,7 @@
 
 local language_manager = {}
 
-local default_language = "en"
+local default_language = "fr"
 
 -- Returns the id of the default language.
 function language_manager:get_default_language()
