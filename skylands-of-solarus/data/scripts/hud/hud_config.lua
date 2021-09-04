ee51de99597d23d4bf5b66ffde86b92bcef557ef
hydralerna <loic.guyader.froggy@gmail.com>
2021-09-04 02:18:41 +0200

    Update hud_config.lua

--- skylands-of-solarus/data/scripts/hud/hud_config.lua
+++ skylands-of-solarus/data/scripts/hud/hud_config.lua
@@ -10,7 +10,16 @@
 -- Negative x or y coordinates mean to measure from the right or bottom
 -- of the screen, respectively.
 
+local x_offset = 68
+
 local hud_config = {
+
+  -- HUD menu (left and right)
+  {
+    menu_script = "scripts/hud/hud_menu",
+    x = 0,
+    y = 0,
+  },
 
 
   -- HUD background (top and bottom bars)
@@ -23,36 +32,36 @@ local hud_config = {
   -- Hearts meter.
   {
     menu_script = "scripts/hud/hearts",
-    x = -90,
-    y = 3,
+    x = -177,
+    y = 16,
   },
 
   -- Money counter.
   {
     menu_script = "scripts/hud/money",
-    x = 2,
+    x = 90,
     y = -14,
   },
 
   -- Rupees counter.
   {
     menu_script = "scripts/hud/rupee",
-    x = 42,
+    x = 130,
     y = -14,
   },
 
   --Level and Experience Counter
   {
     menu_script = ("scripts/hud/lvl_and_exp"),
-    x = 128,
+    x = 216,
     y = -14,
   },
 
   -- Pause icon.
   {
     menu_script = "scripts/hud/pause_icon",
-    x = 92,
-    y = -14,
+    x = x_offset + 12,
+    y = 2,
   },
 
   -- Chrono counter.
@@ -65,35 +74,35 @@ local hud_config = {
   -- Item icon for slot 1.
   {
     menu_script = "scripts/hud/item_icon",
-    x = 0,
-    y = 0,
+    x = x_offset,
+    y = 20,
     slot = 1,  -- Item slot (1 or 2).
   },
 
   -- Item icon for slot 2.
   {
     menu_script = "scripts/hud/item_icon",
-    x = 48,
-    y = 0,
+    x = x_offset + 48,
+    y = 20,
     slot = 2,  -- Item slot (1 or 2).
   },
 
   -- Attack icon.
   {
     menu_script = "scripts/hud/attack_icon",
-    x = 24,
-    y = 0,
-    dialog_x = 24,
-    dialog_y = 0,
+    x = x_offset + 24,
+    y = 20,
+    dialog_x = x_offset + 24,
+    dialog_y = 20,
   },
 
   -- Action icon.
   {
     menu_script = "scripts/hud/action_icon",
-    x = 92,
-    y = 178,
-    dialog_x = 92,
-    dialog_y = 178,
+    x = x_offset + 48,
+    y = 2,
+    dialog_x = x_offset + 48,
+    dialog_y = 2,
   },
 }
 
