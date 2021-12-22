987595301e4419271f8b0f5a9e05fca5be94bb71
hydralerna <loic.guyader.froggy@gmail.com>
2021-12-22 03:13:47 +0100

    Update b_hearts.lua

--- skylands-of-solarus/data/scripts/hud/b_hearts.lua
+++ skylands-of-solarus/data/scripts/hud/b_hearts.lua
@@ -4,19 +4,15 @@ local b_hearts_builder = {}
 
 function b_hearts_builder:new(game, config)
 
-  local b_hearts = {}
+  b_hearts = {} -- TODO: LOCAL
 
   if config ~= nil then
     b_hearts.dst_x, b_hearts.dst_y = config.x, config.y
   end
   b_hearts.surface = sol.surface.create(100, 19)
   b_hearts.empty_heart_sprite = sol.sprite.create("hud/empty_b_heart")
-  b_hearts.nb_max_hearts_displayed = 4
-  b_hearts.nb_current_hearts_displayed = 4
   b_hearts.all_hearts_img = sol.surface.create("hud/b_hearts.png")
-  b_hearts.last_map = game:get_value("last_map")
   b_hearts.is_restarted = false
-  b_hearts.is_dying = false
   b_hearts.display = false
 
 
@@ -29,13 +25,12 @@ function b_hearts_builder:new(game, config)
     -- (this is because the hearts are also used in the savegame menu).
 
     -- After game-over don't show gradually getting the life back.
+    b_hearts.last_map = game:get_value("last_map")
     if b_hearts.starting_life ~= nil then
       b_hearts.boss:set_life(b_hearts.starting_life)
       b_hearts.display = true
       b_hearts.is_restarted = true
     end
-    b_hearts.danger_sound_timer = nil
-    b_hearts.display_timer = nil
     b_hearts:check()
     b_hearts:rebuild_surface()
   end
@@ -54,14 +49,19 @@ function b_hearts_builder:new(game, config)
     else
       local map_id = map:get_id()
       if b_hearts.last_map ~= map_id or b_hearts.is_restarted then
-        game:set_value("last", map_id)
+        local in_boss_room = game:get_value("in_boss_room")
+        if in_boss_room == false then
+          return true
+        else
+          game:set_value("last", map_id)
+        end
+        print(in_boss_room)
         b_hearts.last_map = map_id
-        if map:has_entity("boss") then
+        if map:has_entity("boss") and in_boss_room then
             b_hearts.boss = map:get_entity("boss")
             b_hearts.starting_life = b_hearts.boss:get_life()
             b_hearts.nb_max_hearts_displayed = b_hearts.starting_life
             b_hearts.nb_current_hearts_displayed = b_hearts.starting_life
-            b_hearts.count = math.floor(b_hearts.starting_life / 4)
             b_hearts.display = true
             if b_hearts.is_restarted then
               b_hearts.is_restarted = false
@@ -86,40 +86,27 @@ function b_hearts_builder:new(game, config)
           end
         end
       end
-      if nb_current_hearts == 0 and not b_hearts.is_dying then
-        b_hearts.is_dying = true
-        -- print("b_hearts.is_dying", b_hearts.is_dying)
-        need_rebuild = true
-      end
 
-      -- If we are in-game, play an animation and a sound if the life is low.
+      -- If we are in-game, play an animation if the life is low.
       if game:is_started() then
-        if b_hearts.boss:get_life() <= (b_hearts.nb_max_hearts_displayed / 4)
-            and b_hearts.boss:get_life() ~= 0
-            and not game:is_suspended() then
+        if b_hearts.boss:get_life() <= (b_hearts.nb_max_hearts_displayed / 4) then
           need_rebuild = true
-          if b_hearts.empty_heart_sprite:get_animation() ~= "danger" then
-            b_hearts.empty_heart_sprite:set_animation("danger")
-          end
-          if b_hearts.danger_sound_timer == nil then
-            b_hearts.danger_sound_timer = sol.timer.start(self, 250, function()
-              b_hearts:repeat_danger_sound()
-            end)
-            b_hearts.danger_sound_timer:set_suspended_with_map(true)
+          if b_hearts.boss:get_life() == 0 then
+            if b_hearts.timer == nil then
+              b_hearts.timer = sol.timer.start(self, 4500, function()
+                if b_hearts.empty_heart_sprite:get_animation() ~= "explosion" then
+                  b_hearts.empty_heart_sprite:set_animation("explosion")
+                end
+              end)
+            b_hearts.timer:set_suspended_with_map(true)
+            end
+          elseif b_hearts.empty_heart_sprite:get_animation() ~= "danger" then
+              b_hearts.empty_heart_sprite:set_animation("danger")
           end
         elseif b_hearts.empty_heart_sprite:get_animation() ~= "normal" then
           need_rebuild = true
           b_hearts.empty_heart_sprite:set_animation("normal")
         end
-      end
-      if b_hearts.is_dying and b_hearts.display_timer == nil
-          and b_hearts.count > 0 and not game:is_suspended() then
-          b_hearts.display_timer = sol.timer.start(self, 1500, function()
-            b_hearts.count = b_hearts.count - 1
-            b_hearts.display_timer = nil
-            b_hearts:rebuild_surface()
-          end)
-          b_hearts.display_timer:set_suspended_with_map(true)
       end
     end -- (Display)
 
@@ -135,50 +122,26 @@ function b_hearts_builder:new(game, config)
   end
 
 
-  function b_hearts:repeat_danger_sound()
-
-    if b_hearts.boss:get_life() <= (b_hearts.nb_max_hearts_displayed / 4) then
-
-      sol.audio.play_sound("misc/low_health") -- danger
-      b_hearts.danger_sound_timer = sol.timer.start(b_hearts, 750, function()
-        b_hearts:repeat_danger_sound()
-      end)
-      b_hearts.danger_sound_timer:set_suspended_with_map(true)
-    else
-      b_hearts.danger_sound_timer = nil
-    end
-  end
-
-
   function b_hearts:rebuild_surface()
 
     b_hearts.surface:clear()
     if b_hearts.display then
 
-      -- Gradually remove empty hearts while the boss is dying.
-      if b_hearts.is_dying then
-        local count = b_hearts.count
-        for i = 0, count - 1 do
-          local x, y = (i % 10) * 10, math.floor(i / 10) * 10
-          b_hearts.empty_heart_sprite:draw(b_hearts.surface, x, y)
+      -- Display the hearts.
+      for i = 0, (b_hearts.nb_max_hearts_displayed / 4) - 1 do
+        local x, y = (i % 10) * 10, math.floor(i / 10) * 10
+        b_hearts.empty_heart_sprite:draw(b_hearts.surface, x, y)
+        if i < math.floor(b_hearts.nb_current_hearts_displayed / 4) then
+          -- This heart is full.
+          b_hearts.all_hearts_img:draw_region(27, 0, 9, 9, b_hearts.surface, x, y)
         end
-      else
-        -- Display the hearts.
-        for i = 0, (b_hearts.nb_max_hearts_displayed / 4) - 1 do
-          local x, y = (i % 10) * 10, math.floor(i / 10) * 10
-          b_hearts.empty_heart_sprite:draw(b_hearts.surface, x, y)
-          if i < math.floor(b_hearts.nb_current_hearts_displayed / 4) then
-            -- This heart is full.
-            b_hearts.all_hearts_img:draw_region(27, 0, 9, 9, b_hearts.surface, x, y)
-          end
-        end
-        -- Last fraction of heart.
-        local i = math.floor(b_hearts.nb_current_hearts_displayed / 4)
-        local remaining_fraction = b_hearts.nb_current_hearts_displayed % 4
-        if remaining_fraction ~= 0 then
-          local x, y = (i % 10) * 10, math.floor(i / 10) * 10
-          b_hearts.all_hearts_img:draw_region((remaining_fraction - 1) * 9, 0, 9, 9, b_hearts.surface, x, y)
-        end
+      end
+      -- Last fraction of heart.
+      local i = math.floor(b_hearts.nb_current_hearts_displayed / 4)
+      local remaining_fraction = b_hearts.nb_current_hearts_displayed % 4
+      if remaining_fraction ~= 0 then
+        local x, y = (i % 10) * 10, math.floor(i / 10) * 10
+        b_hearts.all_hearts_img:draw_region((remaining_fraction - 1) * 9, 0, 9, 9, b_hearts.surface, x, y)
       end
     end
   end
