f0daa5061c8e0a51d2ee17801d8ae47fe004df9a
hydralerna <loic.guyader.froggy@gmail.com>
2021-12-29 21:59:00 +0100

    Create heart_container.lua

---
+++ skylands-of-solarus/data/items/heart_container.lua
@@ -0,0 +1,45 @@
+-- Lua script of item "heart container".
+-- This script is executed only once for the whole game.
+
+-- Variables
+local item = ...
+
+-- Event called when the game is initialized.
+function item:on_created()
+
+  self:set_sound_when_picked(nil)
+  self:set_brandish_when_picked(false)
+
+end
+
+function item:on_obtained(variant, savegame_variable)
+
+  local map = self:get_map()
+  local hero = map:get_entity("hero")
+  local x_hero,y_hero, layer_hero = hero:get_position()
+  hero:freeze()
+  hero:set_animation("brandish")
+  sol.audio.play_sound("items/fanfare_heart_container")
+  local heart_container_entity = map:create_custom_entity({
+    name = "brandish_sword",
+    sprite = "entities/items",
+    x = x_hero,
+    y = y_hero - 24,
+    width = 16,
+    height = 16,
+    layer = layer_hero,
+    direction = 0
+    })
+  heart_container_entity:get_sprite():set_animation("heart_container")
+  heart_container_entity:get_sprite():set_direction(0)
+  sol.timer.start(heart_container_entity, 2000, function()
+    hero:set_animation("stopped")
+    map:remove_entities("brandish")
+    hero:unfreeze()
+    local game = self:get_game()
+    game:add_max_life(4)
+    game:set_life(game:get_max_life())
+  end)
+
+end
+
