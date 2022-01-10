c986d66f5fd4b9f62a2c4ac9f9b911d7c5d6bb5d
hydralerna <loic.guyader.froggy@gmail.com>
2022-01-10 23:20:35 +0100

    Create rupee_bag.lua

---
+++ skylands-of-solarus/data/items/rupee_bag.lua
@@ -0,0 +1,66 @@
+-- Lua script of item "coin bag".
+-- This script is executed only once for the whole game.
+
+-- Variables
+local item = ...
+local game = item:get_game()
+
+-- Event called when the game is initialized.
+function item:on_created()
+
+  self:set_savegame_variable("possession_rupee_bag")
+  item:set_sound_when_picked(nil)
+  item:set_sound_when_brandished(nil)
+
+end
+
+function item:on_variant_changed(variant)
+
+  -- Obtaining a rupee bag changes the max amount of rupees.
+  local max_rupees = {50, 100, 300, 500, 1000, 3000, 9999}
+  local amount = max_rupees[variant]
+  if amount == nil then
+    error("Invalid variant '" .. variant .. "' for item 'rupee_bag'")
+  end
+  self:get_game():set_max_rupee(max_rupee)
+
+end
+
+
+function item:on_obtaining()
+  
+  local map = self:get_map()
+  local hero = map:get_entity("hero")
+  local x_hero,y_hero, layer_hero = hero:get_position()
+  hero:freeze()
+  hero:set_animation("brandish")
+  sol.audio.play_sound("items/fanfare_item")
+  local direction = self:get_variant() - 1
+  local rupee_bag_entity = map:create_custom_entity({
+    name = "brandish",
+    sprite = "entities/items",
+    x = x_hero,
+    y = y_hero - 13,
+    width = 16,
+    height = 16,
+    layer = layer_hero + 1,
+    direction = direction
+    })
+  local sprite = rupee_bag_entity:get_sprite()
+  sprite:set_animation("rupee_bag")
+  sprite:set_direction(direction)
+
+end
+
+
+function item:on_obtained(variant)
+
+  local rupee_bonus = {7, 23, 58, 91, 173, 0, 856, 2485}
+  game:add_money(rupee_bonus[variant])
+  local map = self:get_map()
+  local hero = map:get_hero("hero")
+  hero:set_animation("stopped")
+  map:remove_entities("brandish")
+  hero:unfreeze()
+
+end
