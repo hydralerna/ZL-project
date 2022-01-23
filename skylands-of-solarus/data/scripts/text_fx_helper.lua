b9a712d1c2cc2cf41c88dbcf3608b64fad6a65f9
hydralerna <loic.guyader.froggy@gmail.com>
2022-01-23 04:00:00 +0100

    Update text_fx_helper.lua

--- skylands-of-solarus/data/scripts/text_fx_helper.lua
+++ skylands-of-solarus/data/scripts/text_fx_helper.lua
@@ -4,8 +4,10 @@
 
 local text_fx_helper = {}
 
+
 -- Creates a text_surface with all same properties except the color. 
 local function copy_text_surface(src_text_surface, new_color)
+
   -- Create a text surface with the shadow color.
   local new_surface = sol.text_surface.create{
     horizontal_alignment = src_text_surface:get_horizontal_alignment(),
@@ -21,6 +23,7 @@ end
 
 -- Draws the 8 components composing the stroke.
 local function draw_stroke_components(dst_surface, text_surface, x, y, delta)
+
   text_surface:draw(dst_surface, x - delta, y)
   text_surface:draw(dst_surface, x - delta, y - delta)
   text_surface:draw(dst_surface, x - delta, y + delta)
@@ -29,48 +32,63 @@ local function draw_stroke_components(dst_surface, text_surface, x, y, delta)
   text_surface:draw(dst_surface, x + delta, y + delta)
   text_surface:draw(dst_surface, x, y - delta)
   text_surface:draw(dst_surface, x, y + delta)
+
 end
+
 
 -- Draws only the stroke.
 function text_fx_helper:draw_text_stroke(dst_surface, text, stroke_color)
+
   -- Create a text surface with the stroke color.
   local text_stroke = copy_text_surface(text, stroke_color)
 
   -- Draw the 8 texts composing the stroke.
   local x, y = text:get_xy()
   draw_stroke_components(dst_surface, text_stroke, x, y, 1)
+
 end
+
 
 -- Draws the text with a stroke.
 function text_fx_helper:draw_text_with_stroke(dst_surface, text, stroke_color)
+
   -- Draw the stroke with the stroke color.
   self:draw_text_stroke(dst_surface, text, stroke_color)
 
   -- Draw text above the stroke.
   text:draw(dst_surface)
+
 end
+
 
 -- Draws the text with a shadow.
 function text_fx_helper:draw_text_with_shadow(dst_surface, text, shadow_color)
+
   -- Draw the stroke with the stroke color.
   self:draw_text_shadow(dst_surface, text, shadow_color)
 
   -- Draw text above the stroke.
   text:draw(dst_surface)
+
 end
+
 
 -- Draws only the shadow.
 function text_fx_helper:draw_text_shadow(dst_surface, text, shadow_color)
+
   -- Create a text surface with the shadow color.
   local text_shadow = copy_text_surface(text, shadow_color)
 
   -- Draw the text composing the shadow.
   local x, y = text:get_xy()
   text_shadow:draw(dst_surface, x, y + 1)
+
 end
+
 
 -- Draws only the stroke and the shadow.
 function text_fx_helper:draw_stroke_and_shadow(dst_surface, text, stroke_color, shadow_color)
+
   local x, y = text:get_xy()
 
   -- Shadow
@@ -83,15 +101,21 @@ function text_fx_helper:draw_stroke_and_shadow(dst_surface, text, stroke_color, 
 
 end
 
+
 -- Draws the text with the stroke and the shadow.
 function text_fx_helper:draw_text_with_stroke_and_shadow(dst_surface, text, stroke_color, shadow_color)
+
   self:draw_stroke_and_shadow(dst_surface, text, stroke_color, shadow_color)
   text:draw(dst_surface)
+
 end
+
 
 -- Draws the text.
 function text_fx_helper:draw_text(dst_surface, text)
+
   text:draw(dst_surface)
+
 end
 
 return text_fx_helper
