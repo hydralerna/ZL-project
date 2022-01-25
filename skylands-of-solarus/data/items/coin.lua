08074b8bb783623c96d2f24c81284ed7a9edc5f2
hydralerna <loic.guyader.froggy@gmail.com>
2022-01-25 21:17:50 +0100

    Update coin.lua

--- skylands-of-solarus/data/items/coin.lua
+++ skylands-of-solarus/data/items/coin.lua
@@ -16,25 +16,63 @@ function item:on_created()
 end
 
 
+
 -- Event called when the hero gets this item.
 function item:on_obtaining(variant, savegame_variable)
 
   local map = item:get_map()
   local hero = map:get_hero()
-  -- Sound
-  if hero:get_state() == "treasure" then
-    sol.audio.play_sound("items/fanfare_item")
-  else
-    sol.audio.play_sound("items/get_rupee")
-  end
   local amounts = {1, 5, 10, 20, 50, 100, 200, 500}
   local amount = amounts[variant]
+
+  if hero:get_state() == "treasure" then
+    local x_hero,y_hero, layer_hero = hero:get_position()
+    hero:freeze()
+    hero:set_animation("brandish")
+    sol.audio.play_sound("items/fanfare_item")
+    local custom_entity = map:create_custom_entity({
+      name = "brandish",
+      sprite = "entities/items",
+      x = x_hero,
+      y = y_hero - 15,
+      width = 16,
+      height = 16,
+      layer = layer_hero + 1,
+      direction = 0
+     })
+     local sprite = custom_entity:get_sprite()
+     sprite:set_animation("coin")
+     sprite:set_direction(variant - 1)
+     sprite:set_ignore_suspend(true)
+  end
+
+  ------------------
+  --  Add money
+  ------------------
   if amount == nil then
     error("Invalid variant '" .. variant .. "' for item 'coin'")
   end
+  sol.audio.play_sound("items/get_rupee")
   item:get_game():add_money(amount)
-  
+
 end
+
+
+
+function item:on_obtained(variant)
+
+  local map = item:get_map()
+  local hero = map:get_hero()
+  if hero:get_state() == "frozen" then
+    local sprite = map:get_entity("brandish"):get_sprite()
+    hero:set_animation("stopped")
+    sprite:set_ignore_suspend(false)
+    map:remove_entities("brandish")
+    hero:unfreeze()
+  end
+
+end
+
 
 
 -- Event called when a pickable treasure representing this item
