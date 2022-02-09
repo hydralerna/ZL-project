c27562c60ef915dfdf97a2fa2884af76aa1b2c1b
hydralerna <loic.guyader.froggy@gmail.com>
2022-02-09 22:51:29 +0100

    Update rupee.lua

--- skylands-of-solarus/data/items/rupee.lua
+++ skylands-of-solarus/data/items/rupee.lua
@@ -22,7 +22,7 @@ function item:on_obtaining(variant, savegame_variable)
 
   local map = item:get_map()
   local hero = map:get_hero()
-  local amounts = {1, 5, 10, 20, 50, 100, 200, 500}
+  local amounts = {1, 5, 10}
   local amount = amounts[variant]
 
   if hero:get_state() == "treasure" then
