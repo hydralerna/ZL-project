7e81155b3081acc4dbbc6a50dd214754b8e20f81
hydralerna <loic.guyader.froggy@gmail.com>
2022-10-24 02:56:29 +0200

    Create block.lua

---
+++ skylands-of-solarus/data/scripts/meta/block.lua
@@ -0,0 +1,49 @@
+-- Initialize block behavior specific to this quest.
+
+
+-- Include scripts
+require("scripts/multi_events")
+
+-- Variables
+local block_meta = sol.main.get_metatable("block")
+
+-- Add custom entity for a special animation 
+-- (e.g. "falling" if the ground below is a hole)
+function block_meta:on_moved()
+  
+  local animations = {hole = "falling", lava = "burning"}
+  local animation = animations[self:get_ground_below()] or "walking"
+  local sprite = self:get_sprite()
+  if sprite:has_animation(animation) then
+    if animation == "walking" then
+      return
+    else
+      local map = self:get_map()
+      local sprite_name = sprite:get_animation_set()
+      local x, y, layer = self:get_position()
+      local offset_x, offset_y = sprite:get_origin()
+      local width, height = self:get_size()
+      local direction = map:get_hero():get_direction4_to(self)
+      local custom_entity = map:create_custom_entity({
+        name = "fake_block_" .. animation,
+        sprite = sprite_name,
+        x = direction == 0 and x + offset_x or direction == 2 and x - offset_x or x,
+        y = direction == 3 and y + offset_y - offset_x or y,
+        width = width,
+        height = height,
+        layer = layer,
+        direction = 0,
+      })
+      local custom_entity_sprite = custom_entity:get_sprite()
+      custom_entity_sprite:set_direction(0)
+      custom_entity:get_sprite():set_animation(animation, function()
+        custom_entity:remove()
+      end)
+    end
+  else
+    error("\"" .. animation .. "\" animation is missing.")
+  end
+
+end
+
+return true
