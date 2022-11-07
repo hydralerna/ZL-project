c5e23eb743f28bfd317d48f2593792aa9bf518ea
hydralerna <loic.guyader.froggy@gmail.com>
2022-11-07 02:56:52 +0100

    Update block.lua

--- skylands-of-solarus/data/scripts/meta/block.lua
+++ skylands-of-solarus/data/scripts/meta/block.lua
@@ -7,6 +7,40 @@ require("scripts/multi_events")
 -- Variables
 local block_meta = sol.main.get_metatable("block")
 
+
+-- Add a custom entity for higher blocks
+-- (Avoid overlapping problems with the sword)
+function block_meta:on_created()
+
+  local animation = "walking2"
+  local sprite = self:get_sprite()
+  if sprite:has_animation(animation) then
+    local map = self:get_map()
+    local sprite_name = sprite:get_animation_set()
+    local x, y, layer = self:get_position()
+    local width, height = self:get_size()
+    local custom_entity = map:create_custom_entity({
+      name = "",
+      sprite = sprite_name,
+      x = x,
+      y = y,
+      width = width,
+      height = 8,
+      layer = layer + 1,
+      direction = 0,
+    })
+    local custom_entity_sprite = custom_entity:get_sprite()
+    custom_entity_sprite:set_direction(0)
+    custom_entity:get_sprite():set_animation(animation)
+    function self:on_position_changed(x, y, layer)
+      custom_entity:set_position(x, y, layer + 1)
+    end
+    function self:on_removed()
+      custom_entity:remove()
+    end
+  end
+end
+
 -- Add custom entity for a special animation 
 -- (e.g. "falling" if the ground below is a hole)
 function block_meta:on_moved()
@@ -16,7 +50,7 @@ function block_meta:on_moved()
   local sprite = self:get_sprite()
   if sprite:has_animation(animation) then
     if animation == "walking" then
-      return
+        return
     else
       local map = self:get_map()
       local sprite_name = sprite:get_animation_set()
