2dd6396ee40400be7e7906502918793effb2b96b
hydralerna <loic.guyader.froggy@gmail.com>
2023-05-07 01:46:37 +0200

    Update block.lua

--- skylands-of-solarus/data/scripts/meta/block.lua
+++ skylands-of-solarus/data/scripts/meta/block.lua
@@ -45,13 +45,13 @@ end
 -- (e.g. "falling" if the ground below is a hole)
 function block_meta:on_moved()
   
-  local animations = {hole = "falling", lava = "melting"}
+  local animations = {hole = "falling", lava = "melting", deep_water = "sinking"}
   local animation = animations[self:get_ground_below()] or "walking"
   local sprite = self:get_sprite()
   if sprite:has_animation(animation) then
     if animation == "walking" then
         return
-    else
+    else   
       local map = self:get_map()
       local sprite_name = sprite:get_animation_set()
       local x, y, layer = self:get_position()
@@ -60,13 +60,18 @@ function block_meta:on_moved()
       local custom_entity = map:create_custom_entity({
         name = "fake_block_" .. animation,
         sprite = sprite_name,
-        x = direction == 0 and x + 8 or direction == 2 and x - 8 or x,
+        x = direction == 0 and x + (width / 2) or direction == 2 and x - (width / 2) or x,
         y = direction == 3 and y + 13 or direction == 1 and y - 3 or y,
         width = width,
         height = height,
         layer = layer,
         direction = 0,
       })
+      if animation == "melting" or animation == "sinking" then
+        sol.timer.start(map, 250, function()
+           sol.audio.play_sound("splash")
+        end)
+      end  
       local custom_entity_sprite = custom_entity:get_sprite()
       custom_entity_sprite:set_direction(0)
       custom_entity:get_sprite():set_animation(animation, function()
