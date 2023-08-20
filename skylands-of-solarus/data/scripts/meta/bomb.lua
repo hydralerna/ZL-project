4b6cd482a1e60914cb7c1eeeafa03e30bbd86efd
hydralerna <38867772+hydralerna@users.noreply.github.com>
2023-08-20 19:09:33 +0200

    Update bomb.lua

--- skylands-of-solarus/data/scripts/meta/bomb.lua
+++ skylands-of-solarus/data/scripts/meta/bomb.lua
@@ -11,16 +11,26 @@ function bomb_meta:on_created()
 
   local bomb = self
   -- Just a test to get a value from an array. See in /items/bomb.lua, item:get_map():create_bomb{ ...
-  ----local map = bomb:get_map()
-  -- local properties = bomb:get_properties()
-  -- local direction = properties[1]["value"]
-  -- for	k, v in pairs(properties[1]) do
-  --  print(k, v)
-  --end
-  -- print(properties[1]["value"])
+  --[[
+  --local map = bomb:get_map()
+  local properties = bomb:get_properties()
+  local direction = properties[1]["value"]
+  for	k, v in pairs(properties[1]) do
+    print(k, v)
+  end
+  print(properties[1]["value"])
+  --]]
   sol.audio.play_sound("items/bomb_drop")
   local sprite = bomb:get_sprite()
   sprite:set_animation("stopped_on_floor")
   sprite:set_direction(math.random(0, 3))
 end
 
+-- Set the value of "cooldown_active_bomb"
+function bomb_meta:on_removed()
+
+  --local bomb = self
+  --local game = bomb:get_game()
+  --game:set_value("cooldown_active_bomb", false)
+  --game:save()
+end
